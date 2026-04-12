dd8f6dae457fc196419484ab2acc2fcacc81dab9
Rene Guzman <rene.guzman1@snhu.edu>
2026-04-12 14:47:21 -0500

    Module 5 Rest Testing

--- app_server/controllers/api.js
+++ app_server/controllers/api.js
@@ -4,7 +4,21 @@ const Trip = mongoose.model('trips');
 module.exports.tripsList = async function (req, res) {
   try {
     const trips = await Trip.find({});
-    res.json(trips);
+    res.status(200).json(trips);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports.tripsFindById = async function (req, res) {
+  try {
+    const trip = await Trip.findById(req.params.tripId);
+
+    if (!trip) {
+      return res.status(404).json({ message: 'Trip not found' });
+    }
+
+    res.status(200).json(trip);
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ctrlApi = require('../app_server/controllers/api');
 
 router.get('/trips', ctrlApi.tripsList);
+router.get('/trips/:tripId', ctrlApi.tripsFindById);
 
 module.exports = router;
